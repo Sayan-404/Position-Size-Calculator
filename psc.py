4734afc26b79cb3b006726dd4e7150a0d25288a2
Sayan-404 <83273547+Sayan-404@users.noreply.github.com>
2022-12-15 19:02:29 +0530

    Added quick functionality and custom account feature

---
+++ psc.py
@@ -0,0 +1,44 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-e','--entry',type=float,help="Entry Price")
+parser.add_argument('-sl','--stoploss',type=float,help="Stop Loss")
+parser.add_argument('-r','--risk',type=float,help="Total amount you want to risk(in %)")
+parser.add_argument('-a','--account',type=float, help='defines the account size')
+parser.add_argument('-p','--percentage',type=float,help='percentage of stop loss')
+args = parser.parse_args()
+
+
+account = 100.0;
+risk_amount = account * (args.risk/100)
+TP = [1.5,2,2.5,3,4,5]
+
+def sl_percentage():
+    change =  args.entry - args.stoploss if args.entry>args.stoploss else args.stoploss-args.entry
+    loss_percentage = (change/args.entry) * 100 
+    tps(loss_percentage,change)
+
+def tps(loss_percentage,change):
+    k = 1
+    position_size = risk_amount / (loss_percentage/100)
+    print("Position Size : %f" %position_size)
+    for tp in TP:
+        NetProfit = tp*risk_amount
+        takeprofit = (change * tp) + args.entry if args.entry > args.stoploss else args.entry - (change * tp)
+        print("Risk/Ratio: "+str(tp)+" TakeProfit: "+str(takeprofit)+" NetProfit: "+str(NetProfit))
+
+if args.percentage:
+    if args.account:
+        account = args.account
+        risk_amount = account * (args.risk/100)
+    position_size = risk_amount / (args.percentage/100)
+    print("Position Size: %f" %position_size)
+
+elif (args.account):
+    account = args.account
+    risk_amount = account * (args.risk/100)
+    sl_percentage()
+else:
+    sl_percentage()
+
